f8262f57ee6411e3379e1f278e12ecb494ec380e
Obi Onuora <obimarcus11@gmail.COM>
2018-08-17 09:28:44 -0400

    A whole lot of coding

--- main.js
+++ main.js
@@ -1,56 +1,103 @@
-const pictures = [
+
+
+const gameData = [
     {
         name: 'Justin Beiber',
-        guessNames: ['Justin Beiber', 'Eleven, from Stanger Things', 'Lindsay Lohan', 'Aaron Carter'],
+        guessNames: ['Lindsey Lohan', 'Eleven, from Stanger Things', 'Justin Beiber', 'Aaron Carter'],
         image: 'pictures/beiber.jpeg'
     },
     {
         name: 'Britney Spears',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'name', 'Britney Spears', 'name'],
         image: 'pictures/britney.jpeg'
     },
     {
         name: 'Jesica Alba',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'name', 'name', 'Jesica Alba'],
         image: 'pictures/alba.jpeg'
     },
     {
         name: 'Halle Berry',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'Halle Berry', 'name', 'name'],
         image: 'pictures/halle.jpeg'
     },
     {
         name: 'Khaleesi Dragon Queen',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'name', 'name', 'Khaleesi Dragon Queen'],
         image: 'pictures/khaleesi.jpeg'
     },
     {
         name: 'Kobe Bryant',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['Kobe Bryant', 'name', 'name', 'name'],
         image: 'pictures/kobe.jpg'
     },
     {
         name: 'President Obama',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'name', 'name', 'President Obama'],
         image: 'pictures/Obama.jpg'
     },
     {
         name: 'Chef Ramsay',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'Chef Ramsay', 'name', 'name'],
         image: 'pictures/ramsay.jpg'
     },
     {
         name: 'Tom Cruise',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['Tom Cruise', 'name', 'name', 'name'],
         image: 'pictures/tom.jpeg'
     },
     {
         name: 'Will Smith',
-        guessNames: ['correct name', 'false name', 'false name', 'false name'],
+        guessNames: ['name', 'name', 'Will Smith', 'name'],
         image: 'pictures/will.jpg'
     },
 
 ]
 
-const picture = document.querySelector('.picture')
-picture.setAttribute('src', pictures[0].image)
+const picture = document.querySelector('.picture');
+const buttons = document.getElementsByClassName('names');
+let picNumber = 0
+let guessNameNumber = 0
+console.log(document.getElementsByClassName('names'))
+// buttons[0].innerHTML = 'cool beans'
+
+function startRound() {
+    picture.setAttribute('src', gameData[picNumber].image)
+    let arrayOfGuessNames = gameData[picNumber].guessNames;
+    let celebSection = gameData[picNumber]
+    function buttonText() {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].innerHTML = `${arrayOfGuessNames[i]}`
+        }
+
+    }
+    buttonText()
+
+    function winLose() {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', function (e) {
+                let innerText = e.target.innerHTML;
+                let theRightButton = e.target
+                console.log(e.target);
+                if (innerText === celebSection.name) {
+                    theRightButton.style.backgroundColor = 'green'
+                }
+
+            });
+
+        }
+    }
+    // function (e) {
+    //     console.log(e.target)
+    //     if (buttons[i] === gameData[picNumber].name) {
+
+    //     }
+
+    guessNameNumber++;
+    picNumber++;
+    winLose()
+
+}
+
+startRound();
+startRound()
